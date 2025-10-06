36adbb85d6a6fe9320da89fdd8e5a2434e717274
Prince Bhatt <sharmaprince89667@gmail.com>
2025-10-06 10:07:44 +0530

    Docker Files Added, Repo Paused untill Windows 11 Update

---
+++ Docker/Demo MERN App for Docker/App/Client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
+import AboutPage from './pages/AboutPage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* Routes */}
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<AboutPage />}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ Docker/Demo MERN App for Docker/App/Client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-500 text-white p-4 rounded">Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
